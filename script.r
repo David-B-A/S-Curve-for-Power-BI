65808b1516771db9a18eb5cc5b68be1c984c76a3
David Esneyder Bello Angulo (UT Gestión Inmobiliaria) <C1099213@ecopetrol.com.co>
2019-05-02 16:51:19 -0500

    Versión 1.0

--- script.r
+++ script.r
@@ -7,56 +7,68 @@ libraryRequireInstall("plotly")
 
 ################### Actual code ####################
 ############################################
-dataset = data.frame(Value)
-############################################
-dataset$FECHA = as.Date(dataset$SEMANA)
-RealPositions = which(dataset$REAL != "")
+orig_dates = as.Date(as.vector(as.matrix(dates)))
+orig_plan = as.vector(as.matrix(plan))
+orig_real = as.vector(as.matrix(real))
+orig_change_control = as.vector(as.matrix(change_control))
 
-if(range(dataset$PLAN)[2]<=1){
-  dataset$PLAN = dataset$PLAN*100
-}
-if(range(dataset$REAL[RealPositions])[2]<=1){
-  dataset$REAL = dataset$REAL*100
-}
-somos =which(dataset$SOMO != "")
-somos=c(somos,length(dataset$FECHA))
+real_positions = which(orig_real != "")
 
-Dates = as.Date(dataset$FECHA[1:somos[1]])
-Avance = dataset$PLAN[1:somos[1]]
-Linea = c()
-for(i in 1:somos[1]){
-  Linea = c(Linea, "PLAN")
+if(range(orig_plan)[2]<=1){
+  orig_plan = orig_plan*100
+}
+if(range(orig_real[real_positions])[2]<=1){
+  orig_real = orig_real*100
+}
+real_positions = which(orig_real != "")
+
+if(range(orig_plan)[2]<=1){
+  orig_plan = orig_plan*100
+}
+if(range(orig_real[real_positions])[2]<=1){
+  orig_real = orig_real*100
+}
+change_control =which(orig_change_control != "")
+change_control=c(change_control,length(orig_dates))
+
+dates = orig_dates[1:change_control[1]]
+progress = orig_plan[1:change_control[1]]
+base_line = c()
+for(i in 1:change_control[1]){
+  base_line = c(base_line, "PLAN")
 }
 
-for(j in 1:(length(somos)-1)){
-  Dates= c(Dates, dataset$FECHA[(somos[j]+1):somos[j+1]])
-  Avance = c(Avance, dataset$PLAN[(somos[j]+1):somos[j+1]])
-  for(k in (somos[j]+1):(somos[j+1])){
-    Linea = c(Linea, paste("SOMO",j))
+for(j in 1:(length(change_control)-1)){
+  dates= c(dates, orig_dates[(change_control[j]+1):change_control[j+1]])
+  progress = c(progress, orig_plan[(change_control[j]+1):change_control[j+1]])
+  for(k in (change_control[j]+1):(change_control[j+1])){
+    base_line = c(base_line, paste("SOMO",j))
   }
 }
 
-Dates= c(Dates,dataset$FECHA[RealPositions])
-Avance = c(Avance, dataset$REAL[RealPositions])
-for(z in RealPositions){
-  Linea = c(Linea, paste("REAL",j))
+dates= c(dates,orig_dates[real_positions])
+progress = c(progress, orig_real[real_positions])
+for(z in real_positions){
+  base_line = c(base_line, paste("REAL"))
 }
 
+df <- data.frame(dates = dates,
+                 variable_data=progress, Variable_Name=base_line)
 
-df <- data.frame(Dates = Dates,
-                 Variable_Data=Avance, Variable_Name=Linea)
-df$Dates <- as.Date(df$Dates,"%m/%d/%y")
-
-plot <- ggplot(data = df, aes(x=Dates, y=Variable_Data, 
+plot <- ggplot(data = df, aes(x=dates, y=variable_data, 
                            colour=Variable_Name, group=1,
-                           text = paste('Fecha: ', Dates,
+                           text = paste('Fecha: ', dates,
                                         '<br>Linea:', Variable_Name, 
-                                        '<br>Avance:', Variable_Data,'%'))) + 
-        geom_line(size = 1) + labs(colour = "") + xlab("Fecha") + ylab("Avance") + theme_grey(base_size = 20)
+                                        '<br>Avance:', variable_data,'%'))) + 
+        geom_line(size = 1.2) + labs(colour = "") + xlab("Fecha") + ylab("Avance") + theme_grey(base_size = 14)
+
+############################################
 ####################################################
 
 ############# Create and save widget ###############
+############
 p = ggplotly(plot, tooltip = "text")%>%
   layout(hovermode = "x")
+############
 internalSaveWidget(p, 'out.html');
 ####################################################
